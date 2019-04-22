fdd8dfc06c0f85df7abc7ebdb718dbdba6cfdb16
wangyasai <471109213@qq.com>
2019-04-22 16:37:20 +0800

    UPDATE

--- js/sketch.js
+++ js/sketch.js
@@ -1,25 +1,23 @@
 let pg,img;
-var b1, b2, b3;
 var noiseScale = 1000;
 var red,green,blue,c,value;
-var w, h,w_,h_;
-
-
-
-
-
+var w, h;
+var loc;
 
 
 function setup(){
  createCanvas(windowWidth,windowHeight);
  w = windowWidth;
  h = windowHeight;
- pg = createGraphics(w,h);
+ pg = createGraphics(windowWidth,windowHeight);
+
 }
+
 
 function p5LoadImage(dataURL){
   img = loadImage(dataURL);
 }
+
 
 function hexToRgb(hex) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -31,15 +29,12 @@ function hexToRgb(hex) {
 }
 
 
-
-
 function draw(){
   background(options.Background);
-
   pg.background(0);
   pg.fill(255);
   pg.stroke(255);
-  var n = map(width,300,2000,2,1);
+  var n = map(width,300,2000,3,1);
   pg.textSize(int(options.textSize/n));
   pg.strokeWeight(options.textWeight);
   pg.textAlign(CENTER);
@@ -53,86 +48,75 @@ function draw(){
   }else if(type ="text") {
     pg.text(options.Text,width/2,height/2);
   }
-  drawLine();  
+  drawLine(); 
+  // image(pg,0,0); 
   pop();
-
-  if(options.SaveSVG == true){
-    save("waveFont.svg");
-    print ("saved svg");
-    noLoop(); 
-  }
 }
 
 
 function drawLine(){
   pg.loadPixels();
-
   strokeWeight(options.StrokeWeight);
-
   for(var y = 0; y < pg.height; y+=int(options.WaveY)){
     if(type == 'SolidColor'){
       stroke(options.Stroke1);
     }else{
-     var percent = norm(y, 0, pg.height);
-     var between = lerpColor(color(options.Stroke1), color(options.Stroke2), percent);
-     stroke(between);
-   }
+      var percent = norm(y, 0, pg.height);
+      var between = lerpColor(color(options.Stroke1), color(options.Stroke2), percent);
+      stroke(between);
+    }
 
-   beginShape();
-   for(var x = 0 ; x < pg.width; x+=int(options.WaveX)){
-    var loc = (x + y *pg.width)*4;
-    b1 = pg.pixels[loc];
-    b2 = pg.pixels[loc+int(options.WaveX)*4];
-    b3 = pg.pixels[loc+int(options.WaveX)*4*2];
-    var angle = noise(y/1000, x/10000, x)*TWO_PI*options.steepDegree;
+    beginShape();
+    for(var x = 0 ; x < pg.width; x+=int(options.WaveX)){
+      loc = (x + y *pg.width)*4;
+      var angle = noise(y/1000, x/10000, x)*TWO_PI*options.steepDegree;
 
-    red =  pg.pixels[loc];
-    green =  pg.pixels[loc+1];
-    blue = pg.pixels[loc+2];
-    c = color(red, green, blue);
-    value = brightness(c);
+      red =  pg.pixels[loc];
+      green =  pg.pixels[loc+1];
+      blue = pg.pixels[loc+2];
+      c = color(red, green, blue);
+      value = brightness(c);
+
+      if(options.isFill == true){
+        fill(options.Fill);
+      }else{
+        noFill();
+      }
+
+      if(value > options.Brightness){
+        if(options.Smooth == true){
+          if(options.Animation == true){
+            curveVertex(x, y+sin((frameCount/40+x/20+y/50))*10-20);
+          }else{
+            curveVertex(x, y+sin(angle)*10-20);
+          }
+        }else if(options.Smooth == false){
+          if(options.Animation == true){
+            vertex(x, y+sin((frameCount/40+x/20+y/50))*10-20);
+          }else{
+            vertex(x, y+sin(angle)*10-20);
+          }
+        }
+      }else if(value<options.Brightness){
+        vertex(x, y);
+      }else{
+        if(options.Wave == true){
+          vertex(x, y);
+        }else{
+          vertex(x, y);
+        }
+      }
+    }
+    endShape();
 
     if(options.isFill == true){
-      fill(options.Fill);
+      fill(options.Background);
+      stroke(options.Stroke1);
+      strokeWeight(options.StrokeWeight);
+      rect(-10,y,width+10,int(options.WaveY));
     }else{
       noFill();
     }
-
-
-    if(value > options.Brightness){
-      if(options.Smooth == true){
-        if(options.Animation == true){
-          curveVertex(x, y+sin((frameCount/40+x/20+y/50))*10-20);
-        }else{
-          curveVertex(x, y+sin(angle)*10-20);
-        }
-      }else if(options.Smooth == false){
-        if(options.Animation == true){
-          vertex(x, y+sin((frameCount/40+x/20+y/50))*10-20);
-        }else{
-          vertex(x, y+sin(angle)*10-20);
-        }
-      }
-    }else if(value<options.Brightness){
-      vertex(x, y);
-    }else{
-      if(options.Wave == true){
-        vertex(x, y);
-      }else{
-        vertex(x, y);
-      }
-    }
   }
-  endShape();
-
-  if(options.isFill == true){
-    fill(options.Background);
-  }else{
-    noFill();
-  }
-  stroke(options.Stroke1);
-  strokeWeight(options.StrokeWeight/2);
-  rect(0,y,width,int(options.WaveY));
-}
-pg.updatePixels();
+  pg.updatePixels();
 }
