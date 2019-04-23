ac06b1eacf128e2007e67c858f0c42015a2d6aa2
wangyasai <471109213@qq.com>
2019-04-23 16:38:40 +0800

    update

--- js/sketch.js
+++ js/sketch.js
@@ -3,14 +3,16 @@ var noiseScale = 1000;
 var red,green,blue,c,value;
 var w, h;
 var loc;
+let myCanvas;
 
 
 function setup(){
- createCanvas(windowWidth,windowHeight);
- w = windowWidth;
- h = windowHeight;
- pg = createGraphics(windowWidth,windowHeight);
-
+  w = windowWidth;
+  h = windowHeight;
+  myCanvas = createCanvas(w,h);
+  pg = createGraphics(w,h);
+  myCanvas.class("myCanvas");
+  pg.class("graphic");
 }
 
 
@@ -29,33 +31,43 @@ function hexToRgb(hex) {
 }
 
 
+
 function draw(){
+  pixelDensity(1);
   background(options.Background);
   pg.background(0);
   pg.fill(255);
   pg.stroke(255);
-  var n = map(width,300,2000,3,1);
-  pg.textSize(int(options.textSize/n));
-  pg.strokeWeight(options.textWeight);
-  pg.textAlign(CENTER);
+  var n = map(w,300,2000,-20,140);
+  pg.textSize(int(options.textSize+n));
+  pg.strokeWeight(options.textWeight); 
 
-  push();
+
   if(type == "image"){
     push();
     pg.imageMode(CENTER);
-    pg.image(img,width/2, height/2);
+    pg.image(img,w/2, h/2);
     pop();
   }else if(type ="text") {
-    pg.text(options.Text,width/2,height/2);
+    pg.textAlign(CENTER);
+    pg.text(options.Text,w/2,h/2);
+    rectMode(CORNER);
   }
+  smooth();
+  push();
   drawLine(); 
   pop();
+  // if(options.Svg == true){
+  //     save("mySVG.svg");      // give file name
+  //     print ("saved svg");
+  //     noLoop(); 
+  //   }
 }
-
 
 
 function drawLine(){
   pg.loadPixels();
+  pg.pixelDensity(pixelDensity());
   strokeWeight(options.StrokeWeight);
 
   for(var y = 0; y < pg.height; y+=int(options.WaveY)){
@@ -67,9 +79,8 @@ function drawLine(){
       stroke(between);
     }
 
-
     beginShape();
-    for(var x = -20 ; x < pg.width; x+=int(options.WaveX)){
+    for(var x = -20 ; x < pg.width+50; x+=int(options.WaveX)){
       loc = (x + y *pg.width)*4;
       var angle = noise(y/1000, x/10000, x)*TWO_PI*options.steepDegree;
 
@@ -79,22 +90,26 @@ function drawLine(){
       c = color(red, green, blue);
       value = brightness(c);
 
+
+
       if(options.isFill == true){
         fill(options.Fill);
+        strokeWeight(options.strokeWeight/2);
       }else{
         noFill();
       }
 
+
       if(value > options.Brightness){
         if(options.Smooth == true){       
           if(options.Animation == true){
-            curveVertex(x, y+sin((frameCount/40+x/20+y/50))*10-20);
+            curveVertex(x, y+sin((frameCount/(100-int(options.Speed))+x/20+y/50))*10-20);
           }else{
             curveVertex(x, y+sin(angle)*10-20);
           }
         }else if(options.Smooth == false){
           if(options.Animation == true){
-            vertex(x, y+sin((frameCount/40+x/20+y/50))*10-20);
+            vertex(x, y+sin((frameCount/(100-int(options.Speed))+x/20+y/50))*10-20);
           }else{
             vertex(x, y+sin(angle)*10-20);
           }
@@ -105,10 +120,9 @@ function drawLine(){
     }
     endShape();
 
-
     if(options.isFill == true){
       fill(options.Background);
-      rect(0,y,width,options.WaveY);
+      rect(-20,y,width+60,options.WaveY);
     }else{
       noFill();
     }
